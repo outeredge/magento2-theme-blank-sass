98ab4f1c25fb751933a4397bfa258e1c476a3cfa
Sam Turner <sam@digisam.net>
2023-10-18 16:11:09 +0000

    remove more snowdog references

--- registration.php
+++ registration.php
@@ -2,6 +2,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::THEME,
-    'frontend/Snowdog/blank',
+    'frontend/outeredge/blank',
     __DIR__
 );
