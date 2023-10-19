d4db397fb542354b809515344863df1db25e1e83
David Windell <david@outeredgeuk.com>
2023-10-19 10:29:04 +0100

    Update registration.php

--- registration.php
+++ registration.php
@@ -2,6 +2,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::THEME,
-    'frontend/outeredge/blank',
+    'frontend/OuterEdge/blank',
     __DIR__
 );
